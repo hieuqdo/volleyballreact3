f9520b0f666e739049b14a6770e1505da42ae399
Phil Truong <pltruong2507@gmail.com>
2019-03-10 20:07:12 -0700

    api request util

---
+++ src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const path = 'https://rbvc.herokuapp.com/api/';
+
+export default class API {
+
+  static get = async request => await axios
+    .get(path + request)
+    .then(({ data }) => data);
+  
+  static post = async (request, values) => await axios
+    .post(path + request, values)
+    .then(({ data }) => data);
+  
+  static patch = async (request, values) => await axios
+    .patch(path + request, values)
+    .then(({ data }) => data);
+  
+  static delete = async request => await axios
+    .delete(path + request)
+    .then(({ data }) => data);
+}
